bac8af727ea4fe032ff9d82d51401445fb339510
AABattery988 <abrahamalexp@gmail.com>
2024-09-28 12:04:32 -0700

    added basic movement and setup for game

---
+++ player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+
+var SPEED = 5.0
+var JUMP_VELOCITY = 6
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor_only():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("sprint"):
+		SPEED = 7
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("playerLeft", "playerRight", "playerForwards", "playerBackwards")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
