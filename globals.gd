765cebd2171307d9476c44b370a928dfcda39acf
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-11-20 21:47:38 -0800

    oh god, inventory basics

--- globals.gd
+++ globals.gd
@@ -1,19 +1,41 @@
 extends Node
 const item_defs = {
 	"wood":{
-		"displayName":"Wood"
+		"displayName":"Wood",
+		"iconPath":"res://textures/icon.svg",
+		"tool":{
+			"isTool":false,
+			"modelPath":"",
+			"durability": {
+				"current":0,
+				"max":100,
+				"infinite":false
+			},
+		"recipes":[]
+		}
 	},
 	"iron-pickaxe":{
 		"displayName":"Iron Pickaxe",
-		"recipe":[
-			{"name":"iron", "count":5},
-			{"name":"wood", "count":10},
+		"iconPath":"res://textures/notfound.png",
+		"tool":{
+			"isTool":true,
+			"modelPath": "res://2100-base-pickaxe.obj",
+			"durability": {
+				"current": 0,
+				"max": 100,
+				"infinite":false
+			}
+		},
+		"recipes":[
+			[{"name":"iron", "count":5},{"name":"wood", "count":10}]
 		]
 	}
 }
 var inventory = [
-	
+	"iron-pickaxe",
+	"wood",
 ]
+var inventoryNumber = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,4 +44,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	Input.action_press("nextInventoryItem")
+	if(Input.is_action_just_pressed("nextInventoryItem")):
+		inventoryNumber += 1
+	if(Input.is_action_just_pressed("nextInventoryItem")):
+		inventoryNumber -= 1
+	if (inventoryNumber < 0):
+		inventoryNumber = 0;
+	for string in inventory:
+		get_node("/root/Game/Control/PanelContainer/HBoxContainer/Slot" + str(inventory.find(string)+1) + "/Image").set_texture(load(item_defs[string].iconPath));
+	get_node("/root/Game/Control/PanelContainer/HBoxContainer/Slot" + str(inventoryNumber+1)).get_stylebox("panel").border_width_left = 10;
+	for node in get_node("/root/Game/Control/PanelContainer/HBoxContainer").get_children():
+		node.get_stylebox("panel").border_width_left = 0;
